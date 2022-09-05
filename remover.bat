1aaca38e9d192013c3e9598f2720b91789489148
Kristian <81705745+KiloBravoBFE@users.noreply.github.com>
2022-09-05 19:39:44 +0200

    kleiner fix

--- remover.bat
+++ remover.bat
@@ -6,25 +6,25 @@ Echo ------------------------------------------------------------
 pause
 
 cls
-Echo Bitte geben Sie den Ordner an, in den die Juction geht.
+Echo Bitte gib den Ordner an, in den die Juction geht.
 Echo Im Fall von Assetto Corsa Mods ist das der "Shortcut",
 Echo welcher im AC-Mod-Ordner die Verknüpfung zum Modordner darstellt.
-Echo Diesen "Ordner" geben Sie nun bitte im Folgenden an.
+Echo Diesen "Ordner" gib nun bitte an.
 Echo ------------------------------------------------------------------
 set /p jnctnfldr=Bitte hier Directory angeben:
 
 cls
-Echo sind Sie sicher dass Sie die Junction entfernen wollen?
+Echo Bist du sicher dass du die Junction entfernen willst?
 set /p inptJnc=(J)a / (N)ein?
-if %inptJnc% == J rmdir /Q "%jnctnfldr%
-if %inptJnc% == N goto closedwn
+if "%inptJnc%" == J rmdir /Q "%jnctnfldr%"
+if "%inptJnc%" == N goto closedwn
 
 cls 
-Echo Die Junction wurde, sofern möglich, geloescht.
+Echo Die Junction wurde, sofern moeglich, geloescht.
 pause
 exit
 
 :closedwn
-Echo Okay, der Vorgang wurde abgebrochen. Einen schoenen Tag Ihnen!
+Echo Okay, der Vorgang wurde abgebrochen.
 pause
 exit
